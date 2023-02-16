64add1171edaab0ecb2d07772542a23675fd2615
dimakuznetsow <108948435+dimakuznetsow@users.noreply.github.com>
2023-02-16 16:57:14 +0200

    checking env files

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ mongoose
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log(`listening on port ${process.env.PORT}`.blue);
+      console.log(`listening on port ${process.env.PORT}`.magenta);
     });
   })
   .catch((error) => console.log(error));
