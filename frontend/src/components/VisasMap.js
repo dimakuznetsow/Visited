86298353313f0216bbffb595167158355cabc558
dimakuznetsow <108948435+dimakuznetsow@users.noreply.github.com>
2023-03-05 15:57:37 +0200

    Navbar routes changes

--- frontend/src/components/VisasMap.js
+++ frontend/src/components/VisasMap.js
@@ -338,7 +338,8 @@ export default function Map({ setTooltipContent }) {
                               <br />
                               <em>
                                 <strong>
-                                  {d.visaRequirement === "Admission refused"
+                                  {d.visaRequirement &&
+                                  d.visaRequirement === "Admission refused"
                                     ? "Admission refused"
                                     : d.visaRequirement === "Travel restricted"
                                     ? "Travel restricted"
